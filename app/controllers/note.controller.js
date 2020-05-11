aba0ed3b41c72560cd90c0ef372e3d915c696ea7
dev5151 <iamdev5151@gmail.com>
2020-05-12 01:45:25 +0530

    Added Controllers||Routes Structure

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,36 @@
+const Note = require('../models/note.model.js');
+
+// Create and Save a new Note
+exports.create = (req, res) => {
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Create a Note
+    const note = new Note({
+        title: req.body.title || "Untitled Note", 
+        content: req.body.content
+    });
+};
+
+// Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+
+};
